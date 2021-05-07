c4934c78de9194163650854533419f0e899286e7
TheSUPERN0VA <79873066+TheSUPERN0VA@users.noreply.github.com>
2021-05-07 14:19:45 -0600

    Add files via upload

--- tkinter.py
+++ tkinter.py
@@ -5,19 +5,57 @@ playlist = []
 
 def addSong():
     playlist.append(E1.get())
+    E1.delete(0, END)
 
-L1 = Label(top, text="Playlist Generator")
-L1.grid(column = 0, row= 1)
+def printList():
+    print(playlist)
+
+def exportPlaylist():    
+    with open('playlist.txt', 'w') as f:
+        for song in playlist:
+            f.write("%s/n" % song)
+
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
+
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUIs")
+    LMain.grid(column = 0, row = 1)
+    
+    B1Main = Button(text = "Week 1", bg= "white", command = week1)
+    B1Main.grid(column = 0, row = 2)
+    
+    B2Main = Button(text = "Week 2", bg = "white")
+    B2Main.grid(column = 0, row = 3)
+    
+    B3Main = Button(text = "Week 3", bg = "white")
+    B3.Main.grid(column = 0, row = 4)
+
+def week1():
+    clearWindow()
+
+    L1 = Label(top, text="Playlist Generator")
+    L1.grid(column = 0, row= 1)
 
 
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
+    E1 = Entry(top, bd = 5)
+    E1.grid(column = 0, row = 2)
 
 
-B1 = Button(text="Add to playlist", bg = "light blue", command = addSong)
-B1.grid(column = 1, row = 2)
+    B1 = Button(text=" + ", bg = "#B4F9FF", command = addSong)
+    B1.grid(column = 1, row = 2)
 
-B2 = Button(text = "Export List", bg = "white", command = printList)
+    B2 = Button(text = "Export List", bg = "white", command = printList)
+    B2.grid(column=1, row =1)
 
+    B3 = Button(text="Export List", bg = "8a7e42", command = exportPlayList)
+    B3.grid(column = 1, row = 3)
 
-top.mainloop()
+    #Bclear = Button(text="Clear", bg = "white", command= clearWindow)
+    #Bclear.grid(column = 20, row = 40)
+
+if __name__ == "__main__":
+    mainMenu()
+    top.mainloop()
